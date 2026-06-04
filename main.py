8666822aba0cc17f38d87f4fc1d23aa074eae51d
NNael Lamongan <143776342+BayuHamzah19@users.noreply.github.com>
2026-06-04 09:37:06 +0700

    Refactor FastAPI app and update prediction logic
    Removed CORS middleware and adjusted image normalization in the prediction function. Updated response messages for API endpoints.

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.middleware.cors import CORSMiddleware
 import tensorflow as tf
 import numpy as np
 from PIL import Image
 import io
 
-app = FastAPI(title="EcoScan AI API", description="API untuk Klasifikasi Sampah menggunakan Wide ResNet")
+app = FastAPI(title="EcoScan AI API")
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 @tf.keras.utils.register_keras_serializable()
 class CustomNormalizationLayer(tf.keras.layers.Layer):
@@ -21,7 +13,8 @@ class CustomNormalizationLayer(tf.keras.layers.Layer):
         super(CustomNormalizationLayer, self).__init__(**kwargs)
         
     def call(self, inputs):
-        return tf.cast(inputs, tf.float32) / 255.0
+        return inputs 
+
 
 MODEL_PATH = 'model_terbaik.keras'
 
@@ -40,43 +33,33 @@ CLASS_NAMES = ['Kaca', 'Kardus', 'Kertas', 'Logam', 'Organik', 'Plastik', 'Resid
 
 @app.get("/")
 def home():
-    return {
-        "status": "active",
-        "message": "EcoScan AI API is Running smoothly!",
-        "total_classes": len(CLASS_NAMES)
-    }
+    return {"message": "EcoScan API is Running!"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    try:
-        contents = await file.read()
-        image = Image.open(io.BytesIO(contents)).convert('RGB')
-        
-        image = image.resize((224, 224))
-        img_array = tf.keras.preprocessing.image.img_to_array(image)
-        img_array = np.expand_dims(img_array, 0)
+    contents = await file.read()
+    image = Image.open(io.BytesIO(contents)).convert('RGB')
+    
+    image = image.resize((224, 224))
+    img_array = tf.keras.preprocessing.image.img_to_array(image)
+    img_array = np.expand_dims(img_array, 0) 
+    img_array = img_array / 255.0            
 
-        predictions = MODEL.predict(img_array)
-        
-        predicted_index = np.argmax(predictions[0])
-        label = CLASS_NAMES[predicted_index]
-        
-        confidence = float(predictions[0][predicted_index] * 100)
+    predictions = MODEL.predict(img_array)
+    
+    predicted_index = np.argmax(predictions[0])
+    label = CLASS_NAMES[predicted_index]
+    
+    confidence = float(predictions[0][predicted_index] * 100)
 
-        return {
-            "status": "success",
-            "prediction": label,
-            "confidence": f"{confidence:.2f}%",
-            "message": f"Objek berhasil diidentifikasi sebagai sampah {label}."
-        }
-        
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": f"Terjadi kesalahan saat memproses gambar: {str(e)}"
-        }
+    return {
+        "status": "success",
+        "prediction": label,
+        "confidence": f"{confidence:.2f}%",
+        "message": f"Ini terdeteksi sebagai {label}"
+    }
 
 if __name__ == "__main__":
     import uvicorn
-    print("Memulai Server EcoScan pada alamat http://localhost:8000 ...") 
+    print("Memulai Server EcoScan...") 
     uvicorn.run(app, host="0.0.0.0", port=8000)
